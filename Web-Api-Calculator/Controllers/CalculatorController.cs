d32eb7d13d24fffda672264700c172ab2cf68739
raman2021 <A00244354@mycambrian.ca>
2022-02-21 19:13:01 -0500

    Add project files.

---
+++ Web-Api-Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Api_Calculator.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public double Addition(int lValue, int rValue)
+        {
+            return lValue + rValue;
+        }
+
+
+        [HttpGet]
+        public double Subtraction(int lValue, int rValue)
+        {
+            return lValue - rValue;
+        }
+
+
+        [HttpGet]
+        public double Multiplication(int lValue, int rValue)
+        {
+            return lValue * rValue;
+        }
+
+
+        [HttpGet]
+        public double Division(int lValue, int rValue)
+        {
+
+
+            if (lValue != 0 || rValue != 0)
+            {
+                return lValue / rValue;
+
+            }
+            else
+            {
+                throw new ArgumentException();
+                
+
+            }
+
+        }
+    }
+}
